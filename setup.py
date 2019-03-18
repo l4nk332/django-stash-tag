4b5b839b8c2b8a7dd8a368996bd49b3571a25d19
Ian Jabour <l4nk332@gmail.com>
2019-03-17 23:09:38 -0700

    Create setup.py for project

---
+++ setup.py
@@ -0,0 +1,26 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / 'README.md').read_text()
+
+setup(
+    name='django-stash-tag',
+    version='19.3.0',
+    description='A Django Template Tag that stashes content for dynamic reuse.',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/l4nk332/django-stash-tag',
+    author='Ian Jabour',
+    author_email='l4nk332@gmail.com',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='django, templatetag, template, tag, stash, dynamic',
+    packages=find_packages(),
+    install_requires=['django>=1.8']
+)
